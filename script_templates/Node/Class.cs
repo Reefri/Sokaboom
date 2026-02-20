76226d1ddf045515beee9e301931e4f266859d7f
Ethan MASSE <e.masse@student.isartdigital.com>
2026-02-20 15:38:43 +0100

    Initial commit

---
+++ script_templates/Node/Class.cs
@@ -0,0 +1,25 @@
+using _BINDINGS_NAMESPACE_;
+
+// Author : Ethan Masse
+
+namespace Com.IsartDigital.Sokoban 
+{
+	public partial class _CLASS_ : _BASE_
+	{
+		public override void _Ready()
+		{
+
+		}
+
+		public override void _Process(double pDelta)
+		{
+			float lDelta = (float)pDelta;
+
+		}
+
+		protected override void Dispose(bool pDisposing)
+		{
+
+		}
+	}
+}
