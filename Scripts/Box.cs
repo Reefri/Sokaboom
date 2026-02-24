a7e164d17381d4dc5b30c9dbb7b1c095f033ff6e
Ethan FRENARD <e.frenard@student.isartdigital.com>
2026-02-24 17:30:19 +0100

    [ADD] BOx movements

---
+++ Scripts/Box.cs
@@ -0,0 +1,47 @@
+using Godot;
+using System;
+
+public partial class Box : Node
+{
+	[Export] private AnimationPlayer anim;
+	[Export] private GpuParticles2D moveDust;
+
+	private const string GO_UP_ANIM = "goUp";
+	private const string GO_DOWN_ANIM = "goDown";
+	private const string GO_RIGHT_ANIM = "goRight";
+	private const string GO_LEFT_ANIM = "goLeft";
+	public override void _Ready()
+	{
+
+	}
+
+
+	public override void _Process(double delta)
+	{
+
+		if (Input.IsKeyPressed(Key.Z) && !anim.IsPlaying())
+		{
+			anim.Play(GO_UP_ANIM);
+			moveDust.Emitting = true;
+		}
+
+		if (Input.IsKeyPressed(Key.S) && !anim.IsPlaying())
+		{
+			anim.Play(GO_DOWN_ANIM);
+            moveDust.Emitting = true;
+		}
+
+		if (Input.IsKeyPressed(Key.Q) && !anim.IsPlaying())
+		{
+			anim.Play(GO_LEFT_ANIM);
+            moveDust.Emitting = true;
+		}
+
+		if (Input.IsKeyPressed(Key.D) && !anim.IsPlaying())
+		{
+			anim.Play(GO_RIGHT_ANIM);
+            moveDust.Emitting = true;
+		}
+
+	}
+}
