14e4df7db5081047b10fd15501abc73f4b3a34bb
Ethan FRENARD <e.frenard@student.isartdigital.com>
2026-03-02 11:11:28 +0100

    Box Movements

--- Scripts/Box.cs
+++ Scripts/Box.cs
@@ -1,47 +1,53 @@
 using Godot;
 using System;
 
-public partial class Box : Node
+
+// author : Ethan Frenard
+
+namespace Com.IsartDigital.Sokoban
 {
-	[Export] private AnimationPlayer anim;
-	[Export] private GpuParticles2D moveDust;
-
-	private const string GO_UP_ANIM = "goUp";
-	private const string GO_DOWN_ANIM = "goDown";
-	private const string GO_RIGHT_ANIM = "goRight";
-	private const string GO_LEFT_ANIM = "goLeft";
-	public override void _Ready()
+	public partial class Box : Node
 	{
+		[Export] private AnimationPlayer anim;
+		[Export] private GpuParticles2D moveDust;
 
-	}
-
-
-	public override void _Process(double delta)
-	{
-
-		if (Input.IsKeyPressed(Key.Z) && !anim.IsPlaying())
+		private const string GO_UP_ANIM = "goUp";
+		private const string GO_DOWN_ANIM = "goDown";
+		private const string GO_RIGHT_ANIM = "goRight";
+		private const string GO_LEFT_ANIM = "goLeft";
+		public override void _Ready()
 		{
-			anim.Play(GO_UP_ANIM);
-			moveDust.Emitting = true;
+
 		}
 
-		if (Input.IsKeyPressed(Key.S) && !anim.IsPlaying())
-		{
-			anim.Play(GO_DOWN_ANIM);
-            moveDust.Emitting = true;
-		}
 
-		if (Input.IsKeyPressed(Key.Q) && !anim.IsPlaying())
+		public override void _Process(double delta)
 		{
-			anim.Play(GO_LEFT_ANIM);
-            moveDust.Emitting = true;
-		}
 
-		if (Input.IsKeyPressed(Key.D) && !anim.IsPlaying())
-		{
-			anim.Play(GO_RIGHT_ANIM);
-            moveDust.Emitting = true;
-		}
+			if (Input.IsKeyPressed(Key.Z) && !anim.IsPlaying())
+			{
+				anim.Play(GO_UP_ANIM);
+				moveDust.Emitting = true;
+			}
 
+			if (Input.IsKeyPressed(Key.S) && !anim.IsPlaying())
+			{
+				anim.Play(GO_DOWN_ANIM);
+				moveDust.Emitting = true;
+			}
+
+			if (Input.IsKeyPressed(Key.Q) && !anim.IsPlaying())
+			{
+				anim.Play(GO_LEFT_ANIM);
+				moveDust.Emitting = true;
+			}
+
+			if (Input.IsKeyPressed(Key.D) && !anim.IsPlaying())
+			{
+				anim.Play(GO_RIGHT_ANIM);
+				moveDust.Emitting = true;
+			}
+
+		}
 	}
 }
