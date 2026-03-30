7f3e4c6b47645902f8743367b351d26e94f2ea9a
Ethan FRENARD <e.frenard@student.isartdigital.com>
2026-03-30 12:50:19 +0200

    Slide/Door Transition
    added a transition for selecting a level, where two doors slide closed, then re open after a short duration

---
+++ Scripts/UI/Transitions/SlideTransition.cs
@@ -0,0 +1,74 @@
+using Com.IsartDigital.Utils.Tweens;
+using Godot;
+using System.Net;
+
+// Author : Ethan Frenard
+
+namespace Com.IsartDigital.Sokoban 
+{
+	public partial class SlideTransition : Control
+	{
+		[Export] private Node2D leftDoor;
+		[Export] private Node2D rightDoor;
+
+		[Export] private float tweenDuration = 1;
+
+		[Export] private Timer animationTimer;
+		[Export] private float doorClosedDuration = 0.5f;
+
+		private float startingPosFactor;
+		private float time = 0;
+		public override void _Ready()
+		{
+			animationTimer.WaitTime = tweenDuration + doorClosedDuration;
+			animationTimer.Start();
+            animationTimer.Timeout += ReverseAnimation;
+
+			startingPosFactor = GetRect().Size.X / 2;
+
+			leftDoor.Position += Vector2.Left * startingPosFactor;
+			rightDoor.Position += Vector2.Right * startingPosFactor;
+
+			Tween lLeftDoorTween = leftDoor.CreateTween()
+                .SetTrans(Tween.TransitionType.Bounce)
+                .SetEase(Tween.EaseType.Out);
+			lLeftDoorTween.TweenProperty(leftDoor, TweenProp.POSITION,
+				GetRect().Size/2 + Vector2.Left * startingPosFactor / 2, tweenDuration);
+
+            Tween lRightDoorTween = rightDoor.CreateTween()
+                .SetTrans(Tween.TransitionType.Bounce)
+                .SetEase(Tween.EaseType.Out);
+            lRightDoorTween.TweenProperty(rightDoor, TweenProp.POSITION,
+                GetRect().Size / 2 + Vector2.Right * startingPosFactor/2, tweenDuration);
+        }
+
+        private void ReverseAnimation()
+        {
+            Tween lLeftDoorTween = leftDoor.CreateTween()
+                .SetTrans(Tween.TransitionType.Expo)
+                .SetEase(Tween.EaseType.In);
+            lLeftDoorTween.TweenProperty(leftDoor, TweenProp.POSITION,
+                Vector2.Down * GetRect().Size / 2 + Vector2.Left * startingPosFactor, tweenDuration);
+
+            Tween lRightDoorTween = rightDoor.CreateTween()
+                .SetTrans(Tween.TransitionType.Expo)
+                .SetEase(Tween.EaseType.In);
+            lRightDoorTween.TweenProperty(rightDoor, TweenProp.POSITION,
+                (Vector2.Down/2 + Vector2.Right) * GetRect().Size + Vector2.Right * startingPosFactor, tweenDuration);
+        }
+
+        public override void _Process(double pDelta)
+		{
+			float lDelta = (float)pDelta;
+
+			time += lDelta;
+
+			if (time > (tweenDuration + doorClosedDuration) + tweenDuration) QueueFree();
+		}
+
+		protected override void Dispose(bool pDisposing)
+		{
+
+		}
+	}
+}
