72ddf827e0abb688fc022e65a8926b0b8baf767d
Cayot Daniel <d.cayot@student.isartdigital.com>
2026-04-02 21:14:59 +0200

    New fix on Undo

--- Scripts/Manager/GameManager.cs
+++ Scripts/Manager/GameManager.cs
@@ -384,7 +384,7 @@ namespace Com.IsartDigital.Sokoban
 
         public void MoveBackInTime()
         {
-            if (Player.GetInstance().hasBoxToPush) return;
+            if (Player.GetInstance().hasBoxToPush || Box.animPlaying) return;
 
             if (currentPosition.previousValue == null)
             {
