7f1c80626cede2020233d66393d5d72a87871945
Cayot Daniel <d.cayot@student.isartdigital.com>
2026-04-07 21:02:06 +0200

    Banderole & New AI level

--- Scripts/Juiciness/ButtonAnimation.cs
+++ Scripts/Juiciness/ButtonAnimation.cs
@@ -43,10 +43,10 @@ namespace Com.IsartDigital.Sokoban
         {
             SoundManager.GetInstance().PlayClick();
 
-            for(int i = 3; i <= 0; i--)
-            {
-                ClickUI.Create(GetGlobalMousePosition() + new Vector2(-50 + i * 100, -50 + i * 100), UIManager.GetInstance());
-            }
+            //for(int i = 3; i <= 0; i--)
+            //{
+            //    ClickUI.Create(GetGlobalMousePosition() + new Vector2(-50 + i * 100, -50 + i * 100), UIManager.GetInstance());
+            //}
 
             Tween lTween = CreateTween().SetTrans(Tween.TransitionType.Back).SetEase(Tween.EaseType.InOut).SetParallel();
             lTween.TweenProperty(pButton, TweenProp.ROTATION, 100, 1f);
