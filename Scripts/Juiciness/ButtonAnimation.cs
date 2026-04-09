f90fef3ed29dc06180bf9e085e8fa199c6cb2fe5
Ethan MASSE <e.masse@student.isartdigital.com>
2026-04-09 13:50:40 +0200

    Add heritage for button animation

--- Scripts/Juiciness/ButtonAnimation.cs
+++ Scripts/Juiciness/ButtonAnimation.cs
@@ -7,50 +7,11 @@ using System.Linq;
 
 namespace Com.IsartDigital.Sokoban 
 {
-	public partial class ButtonAnimation : Control
-	{
-        RandomNumberGenerator lRand = new RandomNumberGenerator();
-        private int lIndex;
-        private const float DURATION_WHEN_CROSSED = 0.5f;
-
-        private bool pressed;
-
-        public override void _Ready()
-		{
-            List<Node> lChildren = GetChildren().ToList();
-
-            foreach (Button lButtons in lChildren)
-            {
-                lButtons.MouseEntered += () => AnimationMouseEntered(lButtons);
-                lButtons.MouseExited += () => AnimationMouseExited(lButtons);
-                lButtons.Pressed += () => AnimationMousePressed(lButtons);
-
-                lButtons.PivotOffset = lButtons.Size / 2;
-            }
-        }
-
-        private void AnimationMouseEntered(Button pButton)
+	public partial class ButtonAnimation : ButtonAnimMouse
+    {
+        protected override void AnimationMousePressed(Button pButton)
         {
-            if (pButton.Disabled) return;
-            lIndex = lRand.RandiRange(0, 1);
-
-			Tween lTween = CreateTween().SetTrans(Tween.TransitionType.Back).SetEase(Tween.EaseType.OutIn).SetParallel();
-			lTween.TweenProperty(pButton, TweenProp.SCALE, new Vector2(1.1f, 1.1f), DURATION_WHEN_CROSSED);
-            lTween.TweenProperty(pButton, TweenProp.ROTATION, Mathf.DegToRad(lIndex == 0 ? -5 : 5), DURATION_WHEN_CROSSED);
-        }
-
-        private void AnimationMouseExited(Button pButton)
-        {
-            if (!pressed)
-            {
-                Tween lTween = CreateTween().SetParallel();
-                lTween.TweenProperty(pButton, TweenProp.SCALE, new Vector2(1f, 1f), DURATION_WHEN_CROSSED);
-                lTween.TweenProperty(pButton, TweenProp.ROTATION, Mathf.DegToRad(0), DURATION_WHEN_CROSSED);
-            }
-        }
-        private void AnimationMousePressed(Button pButton)
-        {
-            SoundManager.GetInstance().PlayClick();
+            base.AnimationMousePressed(pButton);
             pressed = true;
 
             Tween lTween = CreateTween().SetTrans(Tween.TransitionType.Back).SetEase(Tween.EaseType.InOut).SetParallel();
