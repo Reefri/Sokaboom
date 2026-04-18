e9f436110b2d6431eead91a7d8ab4eb4905c16c2
Sacha Gramatikoff <s.gramatikoff@student.isartdigital.com>
2026-04-18 18:43:41 +0200

    correction bug json build

--- Scripts/JsonReaderWriter.cs
+++ Scripts/JsonReaderWriter.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System.Text.Json;
 using System.Collections.Generic;
+using System.IO;
 
 // Author : Sacha Gramatikoff
 
@@ -8,12 +9,14 @@ namespace Com.IsartDigital.Sokoban
 {
 	public abstract class JsonReaderWriter
 	{
-        private const string JSONFOLDER_PATH = "res://data/";
+        private const string RES_JSONFOLDER_PATH = "res://data/";
+        private const string USER_JSONFOLDER_PATH = "user://data/";
 
 
         public static List<T> ReadJsonToList<T>(string pPath)
         {
-            using Godot.FileAccess lFile = Godot.FileAccess.Open(JSONFOLDER_PATH+pPath, Godot.FileAccess.ModeFlags.Read);
+            using Godot.FileAccess lFile = Godot.FileAccess.Open(USER_JSONFOLDER_PATH+pPath, Godot.FileAccess.ModeFlags.Read) ?? 
+                Godot.FileAccess.Open(RES_JSONFOLDER_PATH + pPath, Godot.FileAccess.ModeFlags.Read);
 
             if (lFile == null)
             {
@@ -38,7 +41,28 @@ namespace Com.IsartDigital.Sokoban
 
         public static void WriteListToJson<T>(string pPath, List<T> pObjectList)
         {
-            using Godot.FileAccess lFile = Godot.FileAccess.Open(JSONFOLDER_PATH + pPath, Godot.FileAccess.ModeFlags.Write);
+             Godot.FileAccess lFile = Godot.FileAccess.Open(USER_JSONFOLDER_PATH + pPath, Godot.FileAccess.ModeFlags.Write);
+
+            if (lFile == null)
+            {
+
+                // Create folder if it doesn't exist
+                if (!DirAccess.Open("user://").DirExists("data"))
+                {
+                    DirAccess.Open("user://").MakeDir("data");
+                }
+
+                string sourcePath = ProjectSettings.GlobalizePath(RES_JSONFOLDER_PATH + pPath);
+                string destPath = ProjectSettings.GlobalizePath(USER_JSONFOLDER_PATH);
+
+
+                File.Copy(sourcePath, destPath, false);
+
+                lFile = Godot.FileAccess.Open(USER_JSONFOLDER_PATH + pPath, Godot.FileAccess.ModeFlags.Write);
+
+            }
+
+
 
             if (lFile == null)
             {
